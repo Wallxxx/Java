a7bf682c7175f3a7abfea1b7e1d8943a9098b226
Wallxxx <tvaibabyshka@gmail.com>
2021-10-13 13:12:07 +0300

    Добавлена мини инструкция

---
+++ WebService/out/artifacts/WebService_jar/tables.sql
@@ -0,0 +1,24 @@
+CREATE TABLE Users (
+                       id integer PRIMARY KEY AUTO_INCREMENT,
+                       name VARChar(100),
+                       surname varchar(100),
+                       patronymic varchar(100),
+                       age int,
+                       phone varchar(100),
+                       address varchar(255)
+);
+
+CREATE TABLE Accounts (
+                          id INTEger PRIMARY KEY AUTO_INCREMENT,
+                          user int,
+                          number varchar(100) UNIQUE,
+                          balance DECIMAL,
+                          FOREIGN KEY (user) REFERENCES Users(id) on DELETE CASCADE on UPDATE CASCADE
+);
+
+CREATE TABLE Cards (
+                       id integer PRIMARY KEY AUTO_INCREMENT,
+                       account int,
+                       number varchar(100) UNIQUE,
+                       FOREIGN KEY (account) REFERENCES Accounts(id) on DELETE CASCADE on UPDATE CASCADE
+);
