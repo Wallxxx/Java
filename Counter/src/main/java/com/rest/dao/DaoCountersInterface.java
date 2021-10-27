e348d2dc7c13fd5996b344f9d0cefeb7261042f3
Wallxxx <tvaibabyshka@gmail.com>
2021-10-27 10:40:27 +0300

    Добавление документации

--- Counter/src/main/java/com/rest/dao/DaoCountersInterface.java
+++ Counter/src/main/java/com/rest/dao/DaoCountersInterface.java
@@ -2,13 +2,52 @@ package com.rest.dao;
 
 import java.util.Set;
 
+/**
+ * Интерфейс DAO.
+ */
 public interface DaoCountersInterface {
 
+    /**
+     * Создание нового счётчика.
+     * @param name имя нового счётчика.
+     */
     void create(String name);
+
+    /**
+     * Инкремент значения счётчика по имени.
+     * @param name имя счётчика.
+     */
     void inc(String name);
+
+    /**
+     * Получение значения счётчика по имени.
+     * @param name имя счётчика.
+     * @return значение.
+     */
     Integer get(String name);
+
+    /**
+     * Удаление счётчика по имени.
+     * @param name имя счётчика.
+     */
     void delete(String name);
+
+    /**
+     * Получение суммы значений всех счётчиков.
+     * @return сумма значений.
+     */
     Integer sum();
+
+    /**
+     * Получение списка имён всех счётчиков.
+     * @return список имён.
+     */
     Set<String> showNames();
+
+    /**
+     * Проверка на наличие счётчика с указанным именем.
+     * @param name имя счётчика.
+     * @return истина, если есть; ложь - если отсутствует.
+     */
     boolean isHave(String name);
 }
--- Counter/src/main/java/com/rest/service/CounterServiceInterface.java
+++ Counter/src/main/java/com/rest/service/CounterServiceInterface.java
@@ -5,12 +5,55 @@ import org.springframework.http.ResponseEntity;
 
 import java.util.Set;
 
+/**
+ * Интерфейс сервиса управления счётчиками.
+ */
 public interface CounterServiceInterface {
+
+    /**
+     * Добавление нового счётчика с уникальным именем.
+     * @param name имя нового счётчика.
+     * @return http статус.
+     */
     ResponseEntity<Counter> create(String name);
+
+    /**
+     * Инкремент счётчика с указанным именем.
+     * @param name имя счётчика
+     * @return http статус.
+     */
     ResponseEntity<Counter> inc(String name);
+
+    /**
+     * Получение значения указанного счётчика.
+     * @param name имя счётчика.
+     * @return значение, http статус.
+     */
     ResponseEntity<Integer> get(String name);
+
+    /**
+     * Удаление указанного счётчика.
+     * @param name имя счётчика.
+     * @return http статус.
+     */
     ResponseEntity<Counter> delete(String name);
+
+    /**
+     * Суммирование значений всех счётчиков.
+     * @return сумма значений, http статус.
+     */
     ResponseEntity<Integer> sum();
+
+    /**
+     * Получение списка имён всех счётчиков.
+     * @return список имён, http статус.
+     */
     ResponseEntity<Set<String>> showNames();
+
+    /**
+     * Проверка на наличие счётчика.
+     * @param name имя счётчика.
+     * @return http статус ("FOUND" или "NOT_FOUND").
+     */
     ResponseEntity<Counter> isHave(String name);
 }
